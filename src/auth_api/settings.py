5713c9211e9affc0fd1bb7c433e65ec4af910d1e
nchalifo <chalifournoah@gmail.com>
2022-09-14 15:35:55 -0700

    Removed hardcoded secret key

--- src/auth_api/settings.py
+++ src/auth_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6-=c7ndc6-2v(k!57fy_+zog3v*0wg&y*#)u144e7^$&(=n$5-'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
